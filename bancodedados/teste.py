a6c37ebc3f6ba9d912632fa9d5c746bb9aec514c
mariacosta77 <costamariaclara421@gmail.com>
2025-11-08 16:44:47 -0300

    ccccccccccc

---
+++ bancodedados/teste.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+try:
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",          # seu usuário MySQL
+        password="123abc"
+        database="usuarios_db"
+)
+    if conn.is_connected():
+        print("✅ Conexão com MySQL bem-sucedida!")
+except mysql.connector.Error as e:
+    print("❌ Erro na conexão:", e)
+finally:
+    if conn.is_connected():
+        conn.close()
